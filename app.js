06cffd195522a156d94f3bf803a295d54fa642c5
Thomas Grainger <tagrain@gmail.com>
2013-02-20 16:54:29 +0000

    fix everything yo ish

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var zlib = require("zlib");
 http.createServer(function(request, response) {
     console.log(request.url);
     var parsed = url.parse(request.url);
-    request.headers["accept-encoding"] = "";
+    //request.headers["accept-encoding"] = "";
     var options = {
         "hostname": parsed.hostname,
         "path": parsed.path,
@@ -24,9 +24,37 @@ http.createServer(function(request, response) {
     } else {
         proxy_request = http.request(options);
     }
+    var in_buffer;
+    var out_buffer;
     proxy_request.on('response', function (proxy_response) {
         response.writeHead(proxy_response.statusCode, proxy_response.headers);
-        proxy_response.pipe(response)
+
+        switch (proxy_response.headers['content-encoding']) {
+            case 'gzip':
+                in_buffer = zlib.createGunzip();
+                proxy_response.pipe(in_buffer);
+                out_buffer = zlib.createGzip();
+                out_buffer.pipe(response);
+                break;
+            case 'deflate':
+                in_buffer = zlib.createInflate();
+                proxy_response.pipe(in_buffer);
+                out_buffer = zlib.createDeflate();
+                out_buffer.pipe(response);
+                break;
+            default:
+                var in_buffer = proxy_response;
+                var out_buffer = response;
+                break;
+        }
+
+        in_buffer.on('data', function(chunk) {
+            console.log(chunk)
+            out_buffer.write(chunk);
+         });
+        in_buffer.on('end', function() {
+            out_buffer.end();
+        });
     });
     request.pipe(proxy_request);
 }).listen(8080);
